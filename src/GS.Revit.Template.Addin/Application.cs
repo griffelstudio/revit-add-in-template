fc2fdcee83587ab65e6a5a046268616aa90d273f
Kiryl A <ibober182@gmail.com>
2022-09-18 23:31:37 +0300

    Fix namespace

--- src/GS.Revit.Template.Addin/Application.cs
+++ src/GS.Revit.Template.Addin/Application.cs
@@ -2,13 +2,26 @@
 
 namespace GS.Revit.Template.Addin
 {
+    /// <summary>
+    /// Revit creates an instance of this class.
+    /// </summary>
     public sealed class Application : IExternalApplication
     {
+        /// <summary>
+        /// Revit calls this method on start up.
+        /// </summary>
+        /// <param name="application">Revit application.</param>
+        /// <returns>Add-in start up result.</returns>
         public Result OnStartup(UIControlledApplication application)
         {
             return Result.Succeeded;
         }
 
+        /// <summary>
+        /// Revit calls this method on shut down.
+        /// </summary>
+        /// <param name="application">Revit application.</param>
+        /// <returns>Add-in shut down result.</returns>
         public Result OnShutdown(UIControlledApplication application)
         {
             return Result.Succeeded;
--- src/GS.Revit.Template.Addin/Command.cs
+++ src/GS.Revit.Template.Addin/Command.cs
@@ -2,11 +2,18 @@
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
-namespace GS.Revit.GltfExporter.API
+namespace GS.Revit.Template.API
 {
+    /// <summary>
+    /// Revit creates an instance of this class.
+    /// </summary>
     [Transaction(TransactionMode.Manual)]
     public class TestCommand : BaseExternalCommand
     {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <returns>Command execution result.</returns>
         public override Result Execute()
         {
             return Result.Succeeded;
